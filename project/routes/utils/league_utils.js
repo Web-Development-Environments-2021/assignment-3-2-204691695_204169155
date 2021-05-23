08203b8e7956e54a89afb7992debb19abec5e626
Tom Nachman <nachmant@post.bgu.ac.il>
2021-05-23 14:49:02 +0300

    added nearest game in league

--- project/routes/utils/league_utils.js
+++ project/routes/utils/league_utils.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
-const LEAGUE_ID = 271;
+const DButils = require("./DButils");
+const LEAGUE_ID = 271; // Superliga
 const api_domain = "https://soccer.sportmonks.com/api/v2.0";
 
 async function getLeagueDetails() {
@@ -21,17 +22,24 @@ async function getLeagueDetails() {
     }
   );
   let games_id = await getMacthIdsBySeason();
- // let games_info = await getFutureGamesInfo(games_id);
-  console.log(games_id);
+  UpdateFutureGamesInfo(games_id);
+  // get latest game
+  const future_games = await DButils.execQuery(`SELECT * FROM dbo.FutureGames`);
+  future_games.sort(function(first, second) {
+    return first.game_timestamp - second.game_timestamp;
+  });
   
 
   return {
     league_name: league.data.data.name,
     current_season_name: league.data.data.season.data.name,
     current_stage_name: stage.data.data.name,
-    // next game details should come from DB
+    next_game: future_games[0],
   };
 }
+
+
+
 // gather future games
 async function getMacthIdsBySeason() {
   let games_ids_list = [];
@@ -49,20 +57,47 @@ async function getMacthIdsBySeason() {
   );
   return games_ids_list;
 }
-async function getFutureGamesInfo(futures_games_ids) {
+async function UpdateFutureGamesInfo(futures_games_ids) {
   let promises = [];
   futures_games_ids.map((id) =>
     promises.push(
       axios.get(`${api_domain}/fixtures/${id}`, {
         params: {
           api_token: process.env.api_token,
-          include: "localTeam,visitorTeam,season,stage",
+          include: "localTeam,visitorTeam",
         },
       })
     )
   );
   let future_games_info = await Promise.all(promises);
-  return extractRelevantFutureGamesData(future_games_info);
+  future_games_info.forEach(element => {
+    const game_id = element.data.data.id;
+    const stage_id = element.data.data.stage_id;
+    const round_id = element.data.data.round_id;
+    const hometeam_id = element.data.data.localteam_id;
+    const hometeam_name = element.data.data.localTeam.data.name
+    const hometeam_logo = element.data.data.localTeam.data.logo_path
+    const vistorteam_id = element.data.data.visitorteam_id;
+    const vistorteam_name = element.data.data.visitorTeam.data.name
+    const vistorteam_logo = element.data.data.visitorTeam.data.logo_path
+    const game_date = element.data.data.time.starting_at.date_time;
+    const game_timestamp = element.data.data.time.starting_at.timestamp;
+    DButils.execQuery(
+      `IF EXISTS(SELECT * from dbo.FutureGames where game_id='${game_id}')
+        UPDATE dbo.FutureGames SET game_date='${game_date}',game_timestamp='${game_timestamp}' WHERE game_id='${game_id}'
+      ELSE
+        INSERT INTO dbo.FutureGames 
+        (game_id, stage_id, round_id, 
+        homeTeam_id, homeTeam_name, homeTeam_logo,
+        visitorTeam_id, visitorTeam_name, visitorTeam_logo,
+        game_date,game_timestamp)
+        VALUES ('${game_id}','${stage_id}','${round_id}',
+        '${hometeam_id}','${hometeam_name}','${hometeam_logo}',
+        '${vistorteam_id}','${vistorteam_name}','${vistorteam_logo}',
+        '${game_date}','${game_timestamp}')`
+      );
+  });
+  return 1;
 }
 
 
